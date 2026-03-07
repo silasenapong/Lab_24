d98282f312b4f2664414aa0b250522a75d0d4520
silasenapong <sila_sanapong@cmu.ac.th>
2026-03-07 16:10:54 +0700

    lab 42_1

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -1,61 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
+class Node
+{
+public:
+	int data;
+	Node *next;
+	~Node();
 };
 
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
+class List
+{
+public:
+	Node *root;
+	int size;
+	void show();
+	void append(int);
+	void insert(int, int);
+	void remove(int);
 };
 
-Node::~Node(){
-    cout << data << " was deleted.\n";
+Node::~Node()
+{
+	cout << data << " was deleted.\n";
 }
 
-void List::insert(int d,int idx){	
+void List::insert(int d, int idx)
+{
+	size++;
 	Node *n = new Node;
 	n->data = d;
-	
-	if(idx == 0){
+
+	if (idx == 0)
+	{
 		n->next = root;
 		root = n;
 		return;
 	}
 	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
+	for (int i = 0; i < idx - 1; i++)
+	{
 		current = current->next;
 	}
 	n->next = current->next;
-	current->next = n;		
+	current->next = n;
 }
 
-void List::show(){
+void List::show()
+{
 	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
+	cout << current->data << " ";
+	while (current->next)
+	{
 		current = current->next;
 		cout << current->data << " ";
-	}	
+	}
 }
 
-void List::append(int d){	
+void List::append(int d)
+{
 	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
+	n->data = d;
+	n->next = NULL;
+	if (root == NULL)
+		root = n;
+	else
+	{
 		Node *current = root;
-		while(current->next){
+		while (current->next)
+		{
 			current = current->next;
 		}
 		current->next = n;
@@ -63,4 +76,62 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int index)
+{
+	Node *current = root;
+
+	if (index == 0)
+	{
+		root = root->next;
+		delete current;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < index - 1; i++)
+		{
+			current = current->next;
+		}
+
+		Node *temp = current->next;
+		current->next = temp->next;
+
+		delete temp;
+		return;
+	}
+}
+
+int main()
+{
+
+	List myList = {0, 0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+	myList.show();
+	cout << "\n";
+
+	myList.insert(29, 2);
+	myList.insert(33, 3);
+	myList.insert(77, 5);
+	myList.insert(69, 0);
+	myList.show();
+	cout << "\n";
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n";
+
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+
+	return 0;
+}
